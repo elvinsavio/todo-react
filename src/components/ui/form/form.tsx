4501792f31a7611ac353bf35bd670b8f22929be7
elvinsavio <elvinsavio.lp@gmail.com>
2023-06-20 19:30:32 +0530

    add framer motion and login page

--- src/components/ui/form/form.tsx
+++ src/components/ui/form/form.tsx
@@ -1,8 +1,19 @@
 import { ReactNode } from 'react';
-
+import { motion } from 'framer-motion';
 interface FormProps {
   children: ReactNode;
+  animate: number;
 }
-export default function Form({ children }: FormProps) {
-  return <form className="flex flex-col gap-3 p-4">{children}</form>;
+export default function Form({ children, animate }: FormProps) {
+  return (
+    <motion.div
+      transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
+      initial={{ x: 200 * animate, height: 'auto' }}
+      animate={{ x: 0 }}
+      exit={{ x: 200 * animate * -1, height: 0 }}
+      className="flex flex-col gap-3 p-4 bg-transparent"
+    >
+      {children}
+    </motion.div>
+  );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,41 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Input from '../components/input/input';
 import { Tabs } from '../components/ui/tab/tabs';
 import Form from '../components/ui/form/form';
 import { Button } from '../components/button/button';
+import { AnimatePresence } from 'framer-motion';
+
+interface ILogin {
+  username: string;
+  password: string;
+  showPassword: boolean;
+}
+
+interface ISignup {
+  username: string;
+  password: string;
+  cPassword: string;
+  showPassword: boolean;
+  showCPassword: boolean;
+}
+
+type tab = 'login' | 'signup';
 
 export default function Login() {
-  type tab = 'login' | 'signup';
   const [tab, setTab] = useState<tab>('login');
 
-  const [login, setLogin] = useState({
+  const [login, setLogin] = useState<ILogin>({
     username: '',
     password: '',
     showPassword: false,
+  });
+
+  const [signup, setSignup] = useState<ISignup>({
+    username: '',
+    password: '',
+    cPassword: '',
+    showPassword: false,
+    showCPassword: false,
   });
 
   return (
@@ -26,26 +50,74 @@ export default function Login() {
             Sign up
           </Tabs.Header>
         </Tabs>
-        <Form>
-          <Input
-            label="Username"
-            type="text"
-            value={login.username}
-            onChange={(value) => setLogin((prev) => ({ ...prev, username: value }))}
-            placeholder="Username"
-          />
-          <Input
-            label="Password"
-            type="password"
-            value={login.password}
-            onChange={(value) => setLogin((prev) => ({ ...prev, password: value }))}
-            placeholder="Password"
-          />
-          <div className="flex">
-            <Button.Submit onClick={() => console.log('click')} />
-          </div>
-        </Form>
+        <AnimatePresence>
+          {tab === 'login' && <LoginForm login={login} setLogin={setLogin} />}
+          {tab === 'signup' && <SignupForm signup={signup} setSignup={setSignup} />}
+        </AnimatePresence>
       </div>
     </div>
   );
 }
+
+interface LoginFormProps {
+  login: ILogin;
+  setLogin: React.Dispatch<React.SetStateAction<ILogin>>;
+}
+const LoginForm = ({ login, setLogin }: LoginFormProps) => {
+  return (
+    <Form animate={1}>
+      <Input
+        label="Username"
+        type="text"
+        value={login.username}
+        onChange={(value) => setLogin((prev: ILogin) => ({ ...prev, username: value }))}
+        placeholder="Username"
+      />
+      <Input
+        label="Password"
+        type="password"
+        value={login.password}
+        onChange={(value) => setLogin((prev) => ({ ...prev, password: value }))}
+        placeholder="Password"
+      />
+      <div className="flex">
+        <Button.Submit onClick={() => console.log('click')} />
+      </div>
+    </Form>
+  );
+};
+
+interface SignupFormProps {
+  signup: ISignup;
+  setSignup: React.Dispatch<React.SetStateAction<ISignup>>;
+}
+const SignupForm = ({ signup, setSignup }: SignupFormProps) => {
+  return (
+    <Form animate={-1}>
+      <Input
+        label="Username"
+        type="text"
+        value={signup.username}
+        onChange={(value) => setSignup((prev) => ({ ...prev, username: value }))}
+        placeholder="Username"
+      />
+      <Input
+        label="Password"
+        type="password"
+        value={signup.password}
+        onChange={(value) => setSignup((prev) => ({ ...prev, password: value }))}
+        placeholder="Password"
+      />
+      <Input
+        label="Confirm Password"
+        type="password"
+        value={signup.cPassword}
+        onChange={(value) => setSignup((prev) => ({ ...prev, cPassword: value }))}
+        placeholder="Password"
+      />
+      <div className="flex">
+        <Button.Submit onClick={() => console.log('click')} />
+      </div>
+    </Form>
+  );
+};
